b074d8146a4a18966dc47b44c34583fd2c76400b
git pessoal <gabriel.san.diogo@gmail.com>
2022-03-22 14:35:01 -0300

    first setup

---
+++ 1- PrimeiroProjeto/1- PrimeiroProjeto/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1__PrimeiroProjeto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("olá mundo, esta é a nossa primeira aplicação em CSharp!! ");
+            Console.ReadLine();
+        }
+    }
+}
